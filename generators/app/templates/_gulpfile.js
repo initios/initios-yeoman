a1c744d2a960ff515b3e89987cef9711e01fdd83
Carlos Goce <carlosgoce@gmail.com>
2015-02-17 18:33:03 +0100

    Add sass concatenation and compilation to css with gulp-sass

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 var config = {
 	"destination": "./build/",
@@ -25,7 +26,16 @@ gulp.task('libJs', function() {
 });
 
 
-
+gulp.task('sass', function() {
+	gulp.src([
+		'./app/assets/sass/app.sass',
+		'./app/assets/sass/**/*.sass'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+  	.pipe(sourcemaps.write('./maps'))
+  	.pipe(gulp.dest(config.destinationCss));
+});
 
 
 gulp.task('production', ['libJs']);
