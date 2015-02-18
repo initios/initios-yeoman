8110cb5288349dde57ed16c12982d9969bf1c1ff
Carlos Goce <carlosgoce@gmail.com>
2015-02-18 15:53:31 +0100

    Bower components moved now to app/assets/vendor/bower_components and update bootstrap file

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,6 +16,7 @@ module.exports = generators.Base.extend({
   		this.directory('app', 'app');
 
   		this.template('_bower.json', 'bower.json');
+  		this.template('_.bowerrc', '.bowerrc');
 		this.template('_package.json', 'package.json');
 		this.template('_gulpfile.js', 'gulpfile.js');
 		this.template('_.gitignore', '.gitignore');
