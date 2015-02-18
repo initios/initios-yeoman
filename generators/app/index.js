600b1b29fc293a8188634ac69ecf698b2b9f61d1
dangarbar <dgarcia@initios.com>
2015-02-18 17:34:17 +0100

    Refactoring

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,30 +5,30 @@ var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
 
-	initializing: function() {
-		this.store = {
-			"bower-dependencies": []
-		}
-	},
+    initializing: function () {
+        this.store = {
+            "bower-dependencies": []
+        }
+    },
 
-  	writing: function() {
-  		// App directory structure
-  		this.directory('app', 'app');
+    writing: function () {
+        // App directory structure
+        this.directory('app', 'app');
 
-  		this.template('_bower.json', 'bower.json');
-  		this.template('_.bowerrc', '.bowerrc');
-		  this.template('_package.json', 'package.json');
-		  this.template('_gulpfile.js', 'gulpfile.js');
-      this.template('_.gitignore', '.gitignore');
-      this.template('_index.jade', './app/index.jade');
-  	},
+        this.template('_bower.json', 'bower.json');
+        this.template('_.bowerrc', '.bowerrc');
+        this.template('_package.json', 'package.json');
+        this.template('_gulpfile.js', 'gulpfile.js');
+        this.template('_.gitignore', '.gitignore');
+        this.template('_index.jade', './app/index.jade');
+    },
 
-  	install: function() {
-		this.installDependencies();
-  	},
+    install: function () {
+        this.installDependencies();
+    },
 
-  	end: function() {
-  		this.log(yosay('Almost done! ' + chalk.green('running default gulp task')));
-		this.spawnCommand('node_modules/gulp/bin/gulp.js');
-  	}
+    end: function () {
+        this.log(yosay('Almost done! ' + chalk.green('running default gulp task')));
+        this.spawnCommand('node_modules/gulp/bin/gulp.js');
+    }
 });
