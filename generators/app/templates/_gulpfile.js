dba48c163c8e75850a39fdec210a543883fcf5c6
dangarbar <dgarcia@initios.com>
2015-02-19 09:14:07 +0100

    Refactoring

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var webserver   = require('gulp-webserver');
+var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var coffee = require('gulp-coffee');
@@ -12,95 +12,95 @@ var gulpif = require('gulp-if');
 var argv = require('yargs').argv;
 
 var config = {
-	"destination": "./build/",
-	"destinationJs": "./build/js/",
-	"destinationCss": "./build/css/",
-	"destinationTemplates": "./build/templates/"
+    "destination": "./build/",
+    "destinationJs": "./build/js/",
+    "destinationCss": "./build/css/",
+    "destinationTemplates": "./build/templates/"
 };
 
-gulp.task('libJs', function() {
-	gulp.src([
-		'./app/assets/vendor/bower_components/jquery/dist/jquery.js',
-		'./app/assets/vendor/bower_components/modernizr/modernizr.js',
-		'./app/assets/vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-		'./app/assets/vendor/bower_components/angular/angular.js'
-	])
-    .pipe(concat('lib.js'))
-	.pipe(gulpif(argv.production, uglify()) )
-	.pipe(gulp.dest(config.destinationJs));
+gulp.task('libJs', function () {
+    gulp.src([
+        './app/assets/vendor/bower_components/jquery/dist/jquery.js',
+        './app/assets/vendor/bower_components/modernizr/modernizr.js',
+        './app/assets/vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+        './app/assets/vendor/bower_components/angular/angular.js'
+    ])
+        .pipe(concat('lib.js'))
+        .pipe(gulpif(argv.production, uglify()))
+        .pipe(gulp.dest(config.destinationJs));
 });
 
-gulp.task('libSass', function() {
-	gulp.src([
-		'./app/assets/styles/lib.sass'
-	])
-	.pipe(gulpif(argv.production,sass({outputStyle: 'compressed'}),sass()))
-  	.pipe(gulp.dest(config.destinationCss));
+gulp.task('libSass', function () {
+    gulp.src([
+        './app/assets/styles/lib.sass'
+    ])
+        .pipe(gulpif(argv.production, sass({outputStyle: 'compressed'}), sass()))
+        .pipe(gulp.dest(config.destinationCss));
 });
 
-gulp.task('sass', function() {
-	gulp.src([
-		'./app/assets/styles/**/*.sass',
-		'!./app/assets/styles/lib.sass'
-	])
-	.pipe(gulpif(argv.production,sass({outputStyle: 'compressed'}),sass()))
-  	.pipe(gulp.dest(config.destinationCss));
+gulp.task('sass', function () {
+    gulp.src([
+        './app/assets/styles/**/*.sass',
+        '!./app/assets/styles/lib.sass'
+    ])
+        .pipe(gulpif(argv.production, sass({outputStyle: 'compressed'}), sass()))
+        .pipe(gulp.dest(config.destinationCss));
 });
 
-gulp.task('coffee', function() {
-	gulp.src('./app/assets/scripts/**/*.coffee')
-	.pipe(coffee({bare: true}).on('error', gutil.log))
-	.pipe(gulp.dest(config.destinationJs))
-	.pipe(gulpif(argv.production, uglify()))
-	.pipe(gulp.dest(config.destinationJs))
-});
- 
-gulp.task('templates', function() {
-  var YOUR_LOCALS = {};
- 
-  // Compile index to html
-  gulp.src([
-  		'./app/index.jade'
-  	])
-	.pipe(jade({
-	  locals: YOUR_LOCALS
-	}))
-    .pipe(gulp.dest(config.destination)
-  );
-
-  // All the rest compiles to js 
-  gulp.src('./app/templates/**/*.jade')
-  .pipe(jade({client: true}))
-  .pipe(gulp.dest(config.destinationTemplates));
+gulp.task('coffee', function () {
+    gulp.src('./app/assets/scripts/**/*.coffee')
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest(config.destinationJs))
+        .pipe(gulpif(argv.production, uglify()))
+        .pipe(gulp.dest(config.destinationJs))
 });
 
+gulp.task('templates', function () {
+    var YOUR_LOCALS = {};
 
-gulp.task('watch', function() {
-	gulp.watch([
-		'./app/assets/styles/**/*.sass',
-		'!./app/assets/styles/lib.sass'
-	], ['sass']);
-	gulp.watch([
-		'./app/assets/styles/lib.sass'
-	], ['libSass']);
-	gulp.watch([
-		'./app/assets/scripts/**/*.coffee'
-	], ['coffee']);
-	gulp.watch([
-		'./app/index.jade',
-		'./app/templates/**/*.jade'
-	], ['templates']);
+    // Compile index to html
+    gulp.src([
+        './app/index.jade'
+    ])
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest(config.destination)
+    );
+
+    // All the rest compiles to js
+    gulp.src('./app/templates/**/*.jade')
+        .pipe(jade({client: true}))
+        .pipe(gulp.dest(config.destinationTemplates));
 });
 
 
-gulp.task('server', function() {
-	gulp.src('./build/')
-		.pipe(webserver({
-			fallback: 'index.html',
-			livereload: true,
-			open: true,
-			port: 9000
-		}));
+gulp.task('watch', function () {
+    gulp.watch([
+        './app/assets/styles/**/*.sass',
+        '!./app/assets/styles/lib.sass'
+    ], ['sass']);
+    gulp.watch([
+        './app/assets/styles/lib.sass'
+    ], ['libSass']);
+    gulp.watch([
+        './app/assets/scripts/**/*.coffee'
+    ], ['coffee']);
+    gulp.watch([
+        './app/index.jade',
+        './app/templates/**/*.jade'
+    ], ['templates']);
+});
+
+
+gulp.task('server', function () {
+    gulp.src('./build/')
+        .pipe(webserver({
+            fallback: 'index.html',
+            livereload: true,
+            open: true,
+            port: 9000
+        }));
 });
 
 
