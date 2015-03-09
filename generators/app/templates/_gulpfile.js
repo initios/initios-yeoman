32177d9db6d029e8f5b2eaa3248f6274cd7ba925
dangarbar <dgarcia@initios.com>
2015-03-09 16:58:20 +0100

    Add multilang support

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
@@ -12,13 +11,16 @@ var gulpif = require('gulp-if');
 var argv = require('yargs').argv;
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var templateCache = require('gulp-angular-templatecache');
+var jsonminify = require('gulp-jsonminify');
 
 var config = {
 	"destination": "./build/",
 	"destinationJs": "./build/js/",
 	"destinationCss": "./build/css/",
 	"destinationImg": "./build/img/",
-	"destinationTemplates": "./build/templates/"
+	"destinationTemplates": "./build/js/",
+    "destinationLang": "./build/i18n/"
 };
 
 gulp.task('libJs', function () {
@@ -26,7 +28,10 @@ gulp.task('libJs', function () {
         './app/assets/vendor/bower_components/jquery/dist/jquery.js',
         './app/assets/vendor/bower_components/modernizr/modernizr.js',
         './app/assets/vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-        './app/assets/vendor/bower_components/angular/angular.js'
+        './app/assets/vendor/bower_components/angular/angular.js',
+        './app/assets/vendor/bower_components/angular-cookies/angular-cookies.js',
+        './app/assets/vendor/bower_components/angular-resource/angular-resource.js',
+        './app/assets/vendor/bower_components/angular-route/angular-route.js'
     ])
         .pipe(concat('lib.js'))
         .pipe(gulpif(argv.production, uglify().on('error', gutil.log)))
@@ -46,7 +51,7 @@ gulp.task('sass', function () {
         './app/assets/styles/**/*.sass',
         '!./app/assets/styles/lib.sass'
     ])
-        .pipe(gulpif(argv.production, sass({outputStyle: 'compressed'}), sass()).on('error', gutil.log))
+        .pipe(gulpif(argv.production, sass({indentedSyntax: true, outputStyle: 'compressed'}), sass({indentedSyntax: true})).on('error', gutil.log))
         .pipe(gulp.dest(config.destinationCss));
 });
 
@@ -77,8 +82,17 @@ gulp.task('templates', function () {
 
     // All the rest compiles to js
     gulp.src('./app/templates/**/*.jade')
-        .pipe(jade({client: true}).on('error', gutil.log))
+        .pipe(jade({pretty: true}).on('error', gutil.log))
+        .pipe(templateCache('templates.js', {standalone:true }))
         .pipe(gulp.dest(config.destinationTemplates));
+});
+
+gulp.task('translations', function () {
+    gulp.src([
+        './app/i18n/*.json',
+    ])
+        .pipe(jsonminify())
+        .pipe(gulp.dest(config.destinationLang))
 });
 
 gulp.task('img', function () {
@@ -109,6 +123,10 @@ gulp.task('watch', function() {
 		'./app/index.jade',
 		'./app/templates/**/*.jade'
 	], ['templates']);
+
+    gulp.watch([
+        './app/i18n/*.json'
+    ], ['translations']);
 	
 	gulp.watch([
 		'./app/assets/img/**/*'
@@ -127,6 +145,6 @@ gulp.task('server', function () {
 });
 
 
-gulp.task('launch', ['server', 'watch']);
-gulp.task('default', ['libJs', 'libSass', 'sass', 'coffee', 'templates', 'img']);
+gulp.task('launch', ['default','server', 'watch']);
+gulp.task('default', ['libJs', 'libSass', 'sass', 'coffee', 'templates', 'translations', 'img']);
  
